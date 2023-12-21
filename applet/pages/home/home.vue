919ed1f772103fc36b94449a6c08266c575b137e
Fufutech <58136868@qq.com>
2023-12-21 20:22:54 +0800

    server

---
+++ applet/pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<uni-grid :column="1" class="paddig">
+		<uni-grid-item>
+			<text class="text">文本</text>
+		</uni-grid-item>
+	</uni-grid>
+</template>
+<script>
+	export default {
+		components: {},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+<style lang="scss">
+
+</style>
